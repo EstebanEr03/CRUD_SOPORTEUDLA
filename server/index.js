da59cb7a13287db4cddb1010e3dd1f237dfca80f
EstebanEr03 <estusgamer@gmail.com>
2024-10-02 05:33:06 -0500

    Se sube con read and create y web responsive

---
+++ server/index.js
@@ -0,0 +1,82 @@
+//Referenciar Express backend node 
+const express = require("express");
+const app = express();
+const mysql = require ("mysql");
+
+
+//cors desbloquea para solicitudes
+const cors= require("cors");
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+  }));  
+
+//middle ware que transforma todo a json
+app.use(express.json());
+
+//conexión db
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"usuariossoporte_crud"
+
+});
+
+//peticion guardar
+app.post("/create",(req,res)=>{
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const sede = req.body.sede;
+    const area = req.body.area;
+
+    db.query('INSERT INTO usuarios(nombre,edad,sede,area) VALUES(?,?,?,?)',[nombre,edad,sede,area],
+        (err,result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send("Usuario registrado con exito!!");
+            }
+        }
+    );
+
+});
+
+//peticion Read
+app.get("/empleados",(req,res)=>{
+    db.query('SELECT * FROM usuarios',
+        (err,result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send(result);
+            }
+        }
+    );
+
+});
+
+//peticion Modificar
+
+app.put("/update",(req,res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const sede = req.body.sede;
+    const area = req.body.area;
+
+    db.query('UPDATE usuarios SET nombre=?,edad=?,sede=?,area=? WHERE id=?',[nombre,edad,sede,area,id],
+        (err,result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send("Usuario actualizado con exito!!");
+            }
+        }
+    );
+
+});
+
+app.listen(3001,()=>{
+    console.log("Corriendo en el puerto 3001")
+})
